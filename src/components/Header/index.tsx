c19d4f0ad73822196bda712148d0bb68fd819f56
Vladislav Remnev <remnevvlad30@gmail.com>
2022-11-28 02:04:52 +0300

    improvement: fixed ghp

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import s from './Header.module.css'
 export const Header = () => {
     return (
         <header className={s.header}>
-            <img src='./img/logo.svg' alt="logo"/>
+            <img src='/img/logo.svg' alt="logo"/>
             <div className={s.phone}>+7 (495) 495-49-54</div>
         </header>
     );
